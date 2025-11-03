58e07066abc8f11bbc29b4f16c13f53a568c8877
Yusuf Kaya <yusuf.kaya1059@gmail.com>
2025-11-03 23:00:59 +0300

    the changes has been fixed

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { route } = require('./src/routes/health');
-
-// routes
-app.use(route);
+const { route } = require('./routes/health');
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 
-
+// routes
+app.use(route);
 
 // listening
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('3000 port is listening');
 })
