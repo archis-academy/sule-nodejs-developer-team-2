07be7f9f313cb4733a44accba08caf2a69313c90
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2025-11-05 01:10:20 +0300

    [SU-37] - clear app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import prisma from './config/db';
 import healthRoutes from './routes/health';
 
 const app = express();
@@ -9,18 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/health', healthRoutes);
 
-async function startServer() {
-  try {
-    await prisma.$connect();
-    console.log('✅ PostgreSQL connection successful via Prisma!');
-    
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 3000}`);
-    });
-  } catch (err) {
-    console.error('❌ PostgreSQL connection failed:', err);
-    process.exit(1);
-  }
-}
-
-startServer();
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
