b3e8a5a3ff9bb0b0bac53a43749ea2e461407850
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2025-11-05 02:21:39 +0300

    [SU-37] - added startServer function for clean app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import healthRoutes from './routes/health';
-import './utils/prisma';
+import { startServer } from './utils/server';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/health', healthRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
+const PORT = parseInt(process.env.PORT || '3000', 10);
+
+startServer(app, PORT);
--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,30 +1,11 @@
 import prisma from '../config/db';
 
-const connectDB = async () => {
-  try {
-    await prisma.$connect();
-    console.log('✅ PostgreSQL connection successful via Prisma!');
-  } catch (err) {
-    console.error('❌ PostgreSQL connection failed:', err);
-    process.exit(1);
-  }
-};
+export async function connectPrisma() {
+  await prisma.$connect();
+  console.log('Prisma connected.');
+}
 
-const disconnectDB = async () => {
+export async function disconnectPrisma() {
   await prisma.$disconnect();
-  console.log('🔌 PostgreSQL connection disconnected.');
-};
-
-connectDB();
-
-process.on('SIGINT', async () => {
-  console.log('SIGINT signal received: Disconnecting from PostgreSQL');
-  await disconnectDB();
-  process.exit(0);
-});
-
-process.on('SIGTERM', async () => {
-  console.log('SIGTERM signal received: Disconnecting from PostgreSQL');
-  await disconnectDB();
-  process.exit(0);
-});
+  console.log('Prisma disconnected.');
+}
