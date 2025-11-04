ba7e6c366daf389bb228621bdb28069950a7ffb7
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2025-11-04 18:13:46 +0300

    Update express and @types/express to latest versions, modify health response message to lowercase

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import healthRoutes from './routes/health';
 
-const app = express();
 dotenv.config();
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/health', healthRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
+  console.log(process.env.PORT);
   console.log('Server is running on port 3000');
 });
