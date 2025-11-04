1c62f20781a6fb89fa43ea9dfee2089f8ae82229
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2025-11-04 18:19:02 +0300

    test commit

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 
 export const getHealth = (req: Request, res: Response) => {
+  console.log('getHealth');
   res.status(200).json({ message: 'ok' });
 };
