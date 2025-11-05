c3c3c6a41e53caadc7906a754b813e5475caa3c6
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2025-11-05 04:58:27 +0300

    [SU-13] - added validation middleware

---
+++ src/middlewares/validation.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
+
+export enum ValidationType {
+  BODY = 'body',
+  PARAMS = 'params',
+  QUERY = 'query',
+}
+
+export const validate = (
+  schema: z.ZodTypeAny,
+  type: ValidationType = ValidationType.BODY
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const dataToValidate = req[type];
+
+      const validatedData = schema.parse(dataToValidate);
+
+      req[type] = validatedData;
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export const validateBody = (schema: z.ZodTypeAny) =>
+  validate(schema, ValidationType.BODY);
+
+export const validateParams = (schema: z.ZodTypeAny) =>
+  validate(schema, ValidationType.PARAMS);
+
+export const validateQuery = (schema: z.ZodTypeAny) =>
+  validate(schema, ValidationType.QUERY);
+
+export const validateId = validate(
+  z.object({
+    id: z
+      .string()
+      .regex(
+        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+        'A valid UUID format is required'
+      ),
+  }),
+  ValidationType.PARAMS
+);
